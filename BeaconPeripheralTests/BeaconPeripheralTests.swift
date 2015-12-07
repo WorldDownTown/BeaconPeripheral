87ae1a004ad383a1b98c1f6db76ad5d8d0293167
WorldDownTown <WorldDownTown@gmail.com>
2015-12-07 17:01:04 +0900

    add Xcode project.

---
+++ BeaconPeripheralTests/BeaconPeripheralTests.swift
@@ -0,0 +1,36 @@
+//
+//  BeaconPeripheralTests.swift
+//  BeaconPeripheralTests
+//
+//  Created by shoji on 2015/12/07.
+//  Copyright © 2015年 com.shoji. All rights reserved.
+//
+
+import XCTest
+@testable import BeaconPeripheral
+
+class BeaconPeripheralTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
